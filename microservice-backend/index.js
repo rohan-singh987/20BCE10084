31993c8dc0f4370a11058c159b5cf3c30a4fbeaf
Rohan Singh <rohannsingh98765@gmail.com>
2023-06-10 01:05:12 +0530

    fetching request
    Signed-off-by: Rohan Singh <rohannsingh98765@gmail.com>

--- microservice-backend/index.js
+++ microservice-backend/index.js
@@ -4,47 +4,6 @@ const axios = require('axios');
 const app = express();
 const port = 8008;
 
-// app.get('/numbers', async (req, res) => {
-//     // localhost:8008/numbers?url=http://104.211.219.98/numbers/primes&url=http://abc.com/fibo
-//   const urls = req.query.url;
-//   console.log(urls);
-// console.log("hihii");
-//   if (!urls) {
-//     return res.status(400).json({ error: 'No URLs provided' });
-//   }
-
-// //   const urlList = Array.isArray(urls) ? urls : [urls];
-
-// //   const numbers = [];
-// //   try {
-// //     for (const url of urlList) {
-// //         console.log(url);
-//       try {
-//         const response = await axios.get(urls);
-//         if (response.status === 200) {
-//           const data = response.data;
-//         //   if (data && Array.isArray(data)) {
-//         //     numbers.push(...data);
-//         //   }
-//         console.log(data);
-//         res.json(data)
-//         }
-//       } catch (error) {
-//         console.error(`Failed to retrieve data from ${urls}`, error);
-//       }
-    
-// //     console.log(numbers);
-// //     console.log("helloooo");
-// //     res.json({ numbers });
-// //   } catch (error) {
-// //     console.error('An error occurred:', error);
-// //     res.status(500).json({ error: 'Internal server error' });
-  
-// });
-
-
-
-
 
 app.get('/numbers', async (req, res) => {
     const urls =req.query.url
@@ -64,7 +23,11 @@ app.get('/numbers', async (req, res) => {
             if (response.status === 200) {
               const data = response.data;
               if (data && Array.isArray(data.numbers)) {
-                return data.numbers;
+                const num = data.numbers.flat();
+                console.log(num);
+                const num1 = num.sort();
+                console.log(num1);
+                return num1;
               }
             }
           } catch (error) {
